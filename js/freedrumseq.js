f04d1c63bec37d4a112d71afaf331a06f210e772
kj7cadd9 <fabercastell679@yahoo.com>
2017-10-25 06:46:48 +0900

    20171025

---
+++ js/freedrumseq.js
@@ -0,0 +1,234 @@
+﻿// 1. Bass Drum			36	//kickbeateron1.wav
+// 2. Snare				38	//snare1.wav
+// 3. Low Floor Tom		41	//floortom1.wav
+// 4. High Floor Tom	43	//tom1.wav
+// 5. Closed Hi-Hat		42	//hihatclose1.wav
+// 6. Pedal Hi-Hat		44	//hihatloose1.wav
+// 7. Open Hi-Hat		46	//hihat1.wav
+// 8. Crash Cymbal		49	//crash20inch1.wav
+
+
+	const bdrm = 36;
+	const srnr = 38
+	const lftm = 41
+	const hftm = 43
+	const cdhh = 42
+	const pdhh = 44
+	const ophh = 46
+	const crsl = 49
+
+	const dm0 = 0;
+	const dm1 = dm0 +1;
+	const dm2 = dm1 +1;
+
+	const dm3 = dm2 +1;
+	const dm4 = dm3 +1;
+	const dm5 = dm4 +1;
+
+	const dm6 = dm5 +1;
+	const dm7 = dm6 +1;
+
+	const SOUNDNUM = dm7 +1;
+
+	var mLocalAudioBuffer= null;
+	var	mAudioBuffer = null;
+	var audioContext = null;	//Use Audio Interface
+
+	var mReadFlag=0;
+	var audioSource = null;
+
+	var mKeylim = Array(SOUNDNUM);
+	var mKeyTotal = 0;
+
+	var mImg_pad = null;
+
+function initdrum(){
+
+	// Web Audio API
+	audioContext = new AudioContext(); //Use Audio Interface
+
+	// Sound Buffer
+	mReadFlag=0;
+	mLocalAudioBuffer= Array(SOUNDNUM);
+	mAudioBuffer = Array(SOUNDNUM);
+
+	for(var i=0; i<SOUNDNUM; i++){
+		mLocalAudioBuffer[i]=new LocalAudioBuffer();
+	}
+
+	//Key Information
+	for(var i=0; i<SOUNDNUM; i++){
+		mKeylim[i]=new Array(3);
+	}
+
+	mKeylim[dm0 ] = [ bdrm,bdrm,bdrm ];
+	mKeylim[dm1 ] = [ srnr,srnr,srnr ];
+	mKeylim[dm2 ] = [ lftm,lftm,lftm ];
+	mKeylim[dm3 ] = [ hftm,hftm,hftm ];
+	mKeylim[dm4 ] = [ cdhh,cdhh,cdhh ];
+	mKeylim[dm5 ] = [ pdhh,pdhh,pdhh ];
+	mKeylim[dm6 ] = [ ophh,ophh,ophh ];
+	mKeylim[dm7 ] = [ crsl,crsl,crsl ];
+
+	mKeyTotal = mKeylim[dm7 ][2] - mKeylim[dm0 ][0]+1;
+	audioSource = Array(mKeyTotal);
+	for(var i=0; i<mKeyTotal; i++){
+		audioSource[i]=null;
+	}
+
+	//Load Wave Files
+	loadDogSound("freedrum/wav/kickbeateron1.wav"	,dm0 );
+	loadDogSound("freedrum/wav/snare1.wav" 			,dm1 );
+	loadDogSound("freedrum/wav/floortom1.wav" 		,dm2 );
+	loadDogSound("freedrum/wav/tom1.wav" 			,dm3 );
+	loadDogSound("freedrum/wav/hihatclose1.wav"		,dm4 );
+	loadDogSound("freedrum/wav/hihatloose1.wav"		,dm5 );
+	loadDogSound("freedrum/wav/hihat1.wav" 			,dm6 );
+	loadDogSound("freedrum/wav/crash20inch1.wav"	,dm7 );
+
+
+	//Load Image File
+	mImg_pad =new Array(9);
+	mPosx =new Array(8);
+	mPosy =new Array(8);
+	for(var i=0; i<9; i++) mImg_pad[i]= new Image();
+	mImg_pad[0].src = "freedrum/png/pad_blueA.png";
+	mImg_pad[1].src = "freedrum/png/pad_blueS.png";
+	mImg_pad[2].src = "freedrum/png/pad_blueD.png";
+	mImg_pad[3].src = "freedrum/png/pad_blueF.png";
+	mImg_pad[4].src = "freedrum/png/pad_greenH.png";
+	mImg_pad[5].src = "freedrum/png/pad_greenJ.png";
+	mImg_pad[6].src = "freedrum/png/pad_greenK.png";
+	mImg_pad[7].src = "freedrum/png/pad_greenL.png";
+	mImg_pad[8].src = "freedrum/png/pad_orange.png";
+
+	// Web MIDI API
+	setInputMenuID(document.input_device_select.ids);
+	setOutputMenuID(document.output_device_select.ids);
+	runTest();
+};
+
+function loadDogSound(url, n) {
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+
+// Decode asynchronously
+	request.onload = function() {
+		audioContext.decodeAudioData(request.response, function(buffer) {
+		mAudioBuffer[n]= buffer; 
+		mLocalAudioBuffer[n].fSetBuffer(mAudioBuffer[n]);
+		mReadFlag++;
+		}, function(){ alert('Error'); } );
+	}
+	request.send();
+}
+
+function handleMIDIMessageGroundpiano( event )
+{
+	var str=null;
+	var status, data1, data2;
+
+	if( event.data[0] ==0xFE ) return;
+	if( event.data[0] ==0xF8 ) return;
+
+	status = event.data[0]&0xF0;
+	data1  = event.data[1];
+	data2  = event.data[2];
+
+	if(status==0x90 && data2==0) status=0x80;
+
+	switch( status ){
+		case 0x80:
+			mNoteoff(data1);
+			break;
+		case 0x90:
+			mNoteon(data1);
+			break;
+		case 0xA0:
+			break;
+		case 0xB0:
+			break;
+		case 0xC0:
+			break;
+		case 0xD0:
+			break;
+		case 0xE0:
+			break;
+		case 0xF0:
+			break;
+	}
+
+}
+
+function mNoteoff( ckey )
+{
+	var dnum=0;
+
+	switch( ckey ){
+		case bdrm: dnum=4; break;
+		case srnr: dnum=3; break;
+		case lftm: dnum=5; break;
+		case hftm: dnum=2; break;
+		case cdhh: dnum=6; break;
+		case pdhh: dnum=1; break;
+		case ophh: dnum=7; break;
+		case crsl: dnum=0; break;
+	}
+
+	fdg1.fDrawImageW(mImg_pad[dnum],mPosx[dnum],mPosy[dnum]);
+	fdg1.fClearWindowInside();
+
+}
+
+function mNoteon( ckey )
+{
+	var cnum=0;
+	var dnum=0;
+	var jnum=ckey- mKeylim[dm0 ][0];
+
+	if( jnum >= mKeyTotal ) return; 
+
+	for(var i=0; i<SOUNDNUM; i++){
+		if( ckey >= mKeylim[i][0] && ckey <= mKeylim[i][2] ) {
+			cnum =i;
+			break;
+		}
+	}
+
+	switch( ckey ){
+		case bdrm: dnum=4; break;
+		case srnr: dnum=3; break;
+		case lftm: dnum=5; break;
+		case hftm: dnum=2; break;
+		case cdhh: dnum=6; break;
+		case pdhh: dnum=1; break;
+		case ophh: dnum=7; break;
+		case crsl: dnum=0; break;
+	}
+
+	fdg1.fDrawImageW(mImg_pad[8],mPosx[dnum],mPosy[dnum]);
+
+	var computedPlaybackRate = Math.pow(2, (ckey-mKeylim[cnum][1])/12);
+
+	audioSource[jnum] = audioContext.createBufferSource();	// creates a sound source
+	audioSource[jnum].buffer = mAudioBuffer[cnum];			// tell the source which sound to play
+	audioSource[jnum].connect(audioContext.destination);
+	audioSource[jnum].playbackRate.value = computedPlaybackRate;
+	audioSource[jnum].start(0);								// play the source now
+
+	fdg1.fDrawLine(mLocalAudioBuffer[cnum].buffer);
+}
+
+
+function process(data){
+
+	var procsize = data.inputBuffer.length;
+
+	/* L-ch を描画する */
+	var inbufL = data.inputBuffer.getChannelData(0);
+	var inbufR = data.inputBuffer.getChannelData(1);
+	var outbufL = data.outputBuffer.getChannelData(0);
+	var outbufR = data.outputBuffer.getChannelData(1);
+
+}
---
+++ js/step_seq.js
@@ -0,0 +1,259 @@
+﻿const mTrack 	=8;			//トラックの数
+const mBeat		=16;		//Beatの数
+var canvas	= null;
+var ctx		= null;
+var log		= null;
+
+var ixb		=10;			//シーケンスのマークの四角の左上のＸの場所
+var iyb		=20;			//シーケンスのマークの四角の左上のＹの場所
+var iyt		=10;			//シーケンスの四角の高さ
+
+var ixo		=40;			//四角の左上のＸの場所
+var iyo		=40;			//四角の左上のＸの場所
+var ixa		=30;			//一つの四角のＸの長さ
+var iya		=40;			//一つの四角のＹの長さ
+
+var toggle  =new Array(mTrack);		//toggle[mTrack][mBeat]
+
+var timerId	=null;
+var ipn		=0;				//Beatの番号
+var tempo	=120;			//テンポの初期値 120
+var beatx	=125;			//16分音符の長さ　ms
+var tone	=new Array();	//各トラックのドラムのノート番号
+var dvol	=[ 127,127,127,127,127,127,127,127 ];	//ドラムの音量
+var mCh		=0;				//Channel Number
+
+var mNumtxt =["1-", "2-", "3-", "4-", "5-", "6-", "7-", "8-"];
+
+window.addEventListener('load', function (){
+
+	var i, j, ix, iy;
+	canvas	= document.getElementById('step_seq');
+	ctx		= canvas.getContext('2d');
+	var clientWidth=canvas.clientWidth;
+
+	ixa=clientWidth/18;
+
+	log = document.getElementById("log");
+
+	ctx.font = "11pt Arial";
+
+	for(i=0; i<mTrack; i++)
+		toggle[i]=new Array(mBeat);
+
+	ctx.fillStyle	= '#DDDDDD';
+	for(i=0; i<mTrack; i++)
+		ctx.fillText(mNumtxt[i], ixo-30, iyo+15+iya*i);
+
+	for(i=0,ix=ixo; i<mBeat; i++){
+		ctx.fillRect(ix, iyb, ixa-2, iyt-2);
+		ix+=ixa;
+	}
+
+	//toggle[mTrack][mBeat]
+	for(j=0,iy=iyo; j<mTrack; j++){
+		for(i=0,ix=ixo; i<mBeat; i++){
+			ctx.fillRect(ix, iy, ixa-2, iya-2);
+			ix+=ixa;
+			toggle[j][i] = false;
+		}
+		iy+=iya;
+	}
+
+	tone[0]=48;		//初期値　Bass Drum
+	tone[1]=52;		//初期値　low Floor Tom
+	tone[2]=53;		//初期値　High Floor Tom
+	tone[3]=55;		//初期値　Closed Hi-Hat
+
+	initdrum();		/* ドラムの初期化 */
+
+	seq_play();		/* シーケンサープ再生スタート */
+
+	button(ixo, iyo, ixa*mBeat, iya*mTrack);
+
+
+});
+
+/* -------------------------------------------------------------------------- */
+function button(x, y, width, height){
+//	ctx.rect(x, y, width, height);
+	var ipx,ipy,ip;
+
+	ctx.stroke();
+
+	canvas.addEventListener('click', function(e){
+		var button = e.target.getBoundingClientRect();
+		mouseX = e.clientX - button.left;
+		mouseY = e.clientY - button.top;
+		ipx = Math.floor((mouseY - iyo)/iya);	//Track
+		ipy = Math.floor((mouseX - ixo)/ixa);	//Beat
+
+/*		log.innerText +="ipx=";
+		log.innerText += ipx;
+		log.innerText +=" ipy=";
+		log.innerText += ipy;
+		log.innerText +="\n";
+*/
+
+		//toggle[mTrack][mBeat], ipx=>Track, ipy=>Beat
+		if(x < mouseX && mouseX < x + width){
+			if(y < mouseY && mouseY < y + height){
+				if(toggle[ipx][ipy]){
+					draw1(ipx,ipy);
+					toggle[ipx][ipy] = true;
+				}else{
+					clear(ipx,ipy);
+					toggle[ipx][ipy] = false;
+				}
+			}
+		}
+	}, false);
+}
+
+/* -------------------------------------------------------------------------- */
+/* シーケンス再生 */
+function seq_play()
+{
+	var jpn;	//Beat
+
+	timerId=setInterval(function(){
+		ctx.fillStyle = "#DDDDDD";
+		ctx.fillRect(ixo+ipn*ixa+1, iyb+1, ixa-2, iyt-1);
+
+		jpn=ipn-1;
+		if(jpn<0) jpn=15;
+
+		//toggle[mTrack][mBeat]
+		if(toggle[0][jpn]==true) outMessage(0x90|mCh,tone[0],0);
+		if(toggle[1][jpn]==true) outMessage(0x90|mCh,tone[1],0);
+		if(toggle[2][jpn]==true) outMessage(0x90|mCh,tone[2],0);
+		if(toggle[3][jpn]==true) outMessage(0x90|mCh,tone[3],0);
+		if(toggle[4][jpn]==true) outMessage(0x90|mCh,tone[4],0);
+		if(toggle[5][jpn]==true) outMessage(0x90|mCh,tone[5],0);
+		if(toggle[6][jpn]==true) outMessage(0x90|mCh,tone[6],0);
+		if(toggle[7][jpn]==true) outMessage(0x90|mCh,tone[7],0);
+
+		if(toggle[0][ipn]==true) outMessage(0x90|mCh,tone[0],dvol[0]);
+		if(toggle[1][ipn]==true) outMessage(0x90|mCh,tone[1],dvol[1]);
+		if(toggle[2][ipn]==true) outMessage(0x90|mCh,tone[2],dvol[2]);
+		if(toggle[3][ipn]==true) outMessage(0x90|mCh,tone[3],dvol[3]);
+		if(toggle[4][ipn]==true) outMessage(0x90|mCh,tone[4],dvol[0]);
+		if(toggle[5][ipn]==true) outMessage(0x90|mCh,tone[5],dvol[1]);
+		if(toggle[6][ipn]==true) outMessage(0x90|mCh,tone[6],dvol[2]);
+		if(toggle[7][ipn]==true) outMessage(0x90|mCh,tone[7],dvol[3]);
+
+		ipn++;
+		if(ipn>=16) ipn=0;
+
+		ctx.fillStyle = "#222222";
+		ctx.fillRect(ixo+ipn*ixa+1, iyb+1, ixa-2, iyt-1);
+
+	}, beatx);
+
+}
+
+/* -------------------------------------------------------------------------- */
+/* 全部消す */
+function clear_all()
+{
+	var i,j;
+	for(j=0; j<mTrack; j++){
+		for(i=0; i<mBeat; i++){
+			toggle[j][i]=true;
+			clear(j,i);
+		}
+	}
+}
+
+/* -------------------------------------------------------------------------- */
+
+function changeTempo(item)
+{
+	tempo = item.value;
+	beatx = Math.floor(60000/tempo/4);
+
+	if(timerId!=null)  clearInterval(timerId);
+	timerId=null;
+
+	seq_play();	/* シーケンサープ再生スタート */
+}
+
+/* -------------------------------------------------------------------------- */
+
+function drumChange(parts,n)
+{
+	tone[n]=parts.selectedIndex+35;
+}
+
+function noteChange(item,n)
+{
+	tone[n]=item.value;
+s}
+
+function changeVolume(item,n)
+{
+	dvol[n]=item.value;
+}
+
+/* -------------------------------------------------------------------------- */
+/* Sequence Start & Stop */
+function seq_stop()
+{
+	if(timerId!=null) clearInterval(timerId);
+	timerId=null;
+	outMessage(0xB0,0x78,0x00);		//All Sound Off
+}
+
+function seq_restart()
+{
+
+	if(timerId!=null)  clearInterval(timerId);
+	timerId=null;
+
+	ctx.clearRect(ixo+ipn*ixa+1, iyb+1, ixa-2, iyt-1);
+	ipn=15;
+
+	seq_play();	/* シーケンサープ再生スタート */
+
+}
+
+/* -------------------------------------------------------------------------- */
+/* 基本図形の描画 */
+//itk=>Track, ibt=>Beat
+function clear(jtk,jbt){
+	ctx.clearRect(ixo+jbt*ixa+1, iyo+jtk*iya+1, 28, 28);
+}
+
+function draw(){
+	ctx.rect(20, 20, 200, 200);
+	ctx.stroke();
+}
+
+/* 円を描く */
+//itk=>Track, ibt=>Beat
+function draw1(jtk,jbt) {
+  ctx.beginPath();
+  ctx.arc(ixo+jbt*ixa+15, iyo+jtk*iya+15, 10, 0, Math.PI*2, false);
+  ctx.stroke();
+}
+
+/* -------------------------------------------------------------------------- */
+var mPch=0;
+var mPMsb=0;
+var mPLsb=0;
+
+function programchange()
+{
+	outMessage(0xB0|mCh,0x00,mPMsb);
+	outMessage(0xB0|mCh,0x20,mPLsb);
+	outMessage2(0xC0|mCh,mPch);
+}
+
+function all_sound_off()
+{
+
+	for(var ich=0; ich<0x10; ich++){
+		outMessage(0xB0|ich,0x78,00);
+	}
+}
+
